8e21d315ecf6448950ad88782db8f56db6b4ab57
b-chae <cbr5964@naver.com>
2021-02-03 17:00:10 +0900

    Hibernate : OneToOne Mappings

---
+++ hb-02-one-to-one-bi/src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -0,0 +1,42 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			System.out.println("Creating new student object...");
+			Instructor tempInstructor = new Instructor("Jaol", "Chae", "cjo@gmail.com");
+			InstructorDetail tempInstructorDetail = new InstructorDetail("http://mailid.com", "netflixandchill");
+			
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+			
+			session.beginTransaction();
+			System.out.println("Saving the instructor...");
+			session.save(tempInstructor);
+			session.getTransaction().commit();
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+
+	}
+
+}
