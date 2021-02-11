5d5c8957da02b9209946df4f2e214df6412690f5
b-chae <cbr5964@naver.com>
2021-02-11 23:39:59 +0900

    Spring REST - CRUD Database Project

---
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.luv2code.springdemo.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomerRestExceptionHandler {
+	
+	
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(Exception exc){
+		
+		CustomerErrorResponse error = new CustomerErrorResponse
+				(HttpStatus.BAD_REQUEST.value(), exc.getMessage(), System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+
+}
